7cb5c218cf4607d4d85185087ba5d44da5f56f19
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2021-06-11 19:01:45 +0300

    asd

--- src/js/example.js
+++ src/js/example.js
@@ -1,1 +1,23 @@
-console.log(' this is example!')
+const abc = document.querySelector('body')
+
+window.addEventListener('resize', () => {
+
+	getFontSize(abc);
+	
+});
+
+
+
+
+const getFontSize = (item)=> 
+{
+	let compStyles = window.getComputedStyle(item);
+
+	const printFontSize = document.querySelector('.print-font-size')
+	printFontSize.textContent = compStyles.getPropertyValue('font-size') ;
+}
+
+getFontSize(abc)
+
+
+
